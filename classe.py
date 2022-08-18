90f19d6cc5c678aa56009bb3eb2e576118a54fdc
José Wellington <109475692+wellington067@users.noreply.github.com>
2022-08-17 22:04:50 -0300

    Create classe.py

---
+++ classe.py
@@ -0,0 +1,11 @@
+"""
+Crie uma classe de sua preferência com, no mínimo, uma variável, um método e um incremento.
+Depois, desenvolva três ou mais objetos para testar o código.
+"""
+
+class carro:
+  totalcarros = 20
+  def vende_carro():
+    
+    
+  
