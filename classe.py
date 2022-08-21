952de8e41faaab5ee74706a827a6c0b4b105aa45
José Wellington <109475692+wellington067@users.noreply.github.com>
2022-08-21 17:22:37 -0300

    Update classe.py

--- classe.py
+++ classe.py
@@ -4,15 +4,13 @@ Depois, desenvolva três ou mais objetos para testar o código.
 """
 
 class carro:
-  vendidos = 0
-  carrosvendidos()
-    totalvendidos++
-    
-    return totalvendidos
-    
-
-
-
-    
-    
+  def __init__(self, ano, cor, lugares,):
+    self.ano = ano
+    self.cor = cor
+    self.lugares = lugares
+if __name__ == '__main__':
+  carro1 = carro('2022', 'preto', '5')
+  carro2 = carro('2020', 'branco', '5')
+  carro3 = carro('2019', 'vermelho', '5')
+  carro4 = carro('2022', 'azul', '5')
   
